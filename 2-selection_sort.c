226f0663b8e2e13c0d84a68c566e04f799d7872f
Mareeam89 <mareeamraj@gmail.com>
2024-10-31 18:27:10 +0100

    update

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,35 +1,32 @@
 #include "sort.h"
-
 /**
- * selection_sort - choose the min number and swap between the initial
- *
- * @array: array of integers
- * @size: size of the array
- * Return: void
+ * selection_sort - function that sorts an array
+ *@size:  size of array
+ * @array: list with numbers
  */
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j, index, swap;
-	int little;
+	size_t i, index;
+	int tmp, swap, flag = 0;
 
-	if (size <= 1)
+	if (array == NULL)
 		return;
-
-	for (j = 0; j < size; j++)
+	for (i = 0; i < size; i++)
 	{
-		/* step1: while through the array to find the small number */
-		swap = 0;
-		little = array[j];
-		for (i = j; i < size; i++)
+		tmp = i;
+		flag = 0;
+		for (index = i + 1; index < size; index++)
 		{
-			if (array[i] < little)
-				little = array[i], index = i, swap = 1;
+			if (array[tmp] > array[index])
+			{
+				tmp = index;
+				flag += 1;
+			}
 		}
-		/* swap */
-		if (swap == 1)
-		{
-			array[index] = array[j];
-			array[j] = little;
+		swap = array[i];
+		array[i] = array[tmp];
+		array[tmp] = swap;
+		if (flag != 0)
 			print_array(array, size);
-		}
 	}
+}
