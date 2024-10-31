80ab86446ee66e499ea9f9e5974c17cff20ea1c2
Mareeam89 <mareeamraj@gmail.com>
2024-10-31 18:13:56 +0100

    update

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,22 +1,4 @@
 #include "sort.h"
-<<<<<<< HEAD
-/**
- * shell_sort -Sort an Array using shell sort
- * @array: array of Int
- * @size: array size
- **/
-
-void shell_sort(int *array, size_t size)
-{
-	unsigned int gap = 1, i, j;
-	int temp;
-
-	if (array == NULL)
-		return;
-	if (size < 2)
-		return;
-	while (gap < size / 3)
-=======
 
 /**
  * _swap - swap between 2 pointers of int
@@ -50,30 +32,14 @@ void shell_sort(int *array, size_t size)
 
 	/* calculate the gap */
 	while ((3 * gap + 1) <= size)
->>>>>>> 05e0411aea3d55c5f3af05eb23078fd12a963ceb
 		gap = gap * 3 + 1;
 
 	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)
-<<<<<<< HEAD
-		{
-			temp = array[i];
-			j = i;
-			while (j >= gap && array[j - gap] > temp)
-			{
-				array[j] = array[j - gap];
-				j -= gap;
-			}
-			array[j] = temp;
-		}
-		print_array(array, size);
-		gap /= 3;
-=======
 			for (j = i; j >= gap && array[j - gap] > array[j] ; j = j - gap)
 				_swap(&array[j], &array[j - gap]);
 		gap = (gap - 1) / 3;
 		print_array(array, size);
->>>>>>> 05e0411aea3d55c5f3af05eb23078fd12a963ceb
 	}
 }
